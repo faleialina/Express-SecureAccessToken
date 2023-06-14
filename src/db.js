bf9463a8f41941a2c26fed6d86a424279ebb1525
faleialina <august3@mail.ru>
2023-06-14 21:43:17 +0300

    Express-SecureAccessToken/post

---
+++ src/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg')
+
+
+const pool = new Pool({
+    user: process.env.DB_user,
+    host: process.env.DB_host,
+    database: process.env.DB_database,
+    password: process.env.DB_password,
+    port: process.env.DB_port,
+});
+module.exports = { pool }
